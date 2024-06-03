f7b42ebb432042cdaf8154fc764fc3a4375f6099
simon9834 <hloska@spsejecna.cz>
2024-06-03 09:55:52 +0200

    first method done ig

---
+++ src/Class.java
@@ -0,0 +1,29 @@
+public class Class {
+
+    public String spinAround(String[] pole) {
+        String whereSpin;
+        int spin = 0;
+        int fullSpinCount = 0;
+        String returnString = "";
+        for (int i = 0; i < pole.length; i++) {
+            whereSpin = pole[i];
+            if (whereSpin.equalsIgnoreCase("left")) {
+                spin += 90;
+                returnString += "u spun left, ";
+            } else if (whereSpin.equalsIgnoreCase("right")) {
+                spin -= 90;
+                returnString += "u spun right, ";
+            }
+            if (spin == -360 || spin == 360){
+                fullSpinCount++;
+                spin -= 360;
+            }
+        }
+        return returnString + "\n360 spins count: " + fullSpinCount;
+    }
+
+    public void vowels(String string, int vowelsCount){
+        char[] toCharArray()
+        string.toCharArray();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Class cl = new Class();
+        String[] pole = new String[6];
+        pole[0] = "right";
+        pole[1] = "right";
+        pole[2] = "right";
+        pole[3] = "right";
+        pole[4] = "left";
+        pole[5] = "right";
+
+        System.out.println(cl.spinAround(pole));
+
+
+
+    }
+}
